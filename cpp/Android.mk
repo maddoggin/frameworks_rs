maddoggin/frameworks_rs

commit 51dc7a27a8749d4ad43169bf39d4378529e132cd

Clang: Silence warnings
With GCC >=4.7 Clang generates a lot of noise with various compiler flags,
and can even break the builds when using Flags like Graphite.  This can be
reproduced by doing the following:

Setting TARGET_EXTRA_CFLAGS with Graphite flags such as these
TARGET_EXTRA_CFLAGS +=  -fgraphite-identity \
                        -floop-block \
                        -floop-strip-mine \
                        -ftree-loop-distribution \
                        -ftree-loop-linear

Causes the following errors to occure and the builds to fail:

clang: error: argument unused during compilation: '-fgraphite-identity'
                                                  '-floop-block'
                                                  '-floop-strip-mine'
                                                  '-ftree-loop-distribution'
                                                  '-ftree-loop-linear'

Silence the errors by passing the -Qunused-arguments for CFLAGS

Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

cpp/Android.mk
@@ -34,6 +34,8 @@ LOCAL_MODULE:= libRScpp
 
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_CFLAGS := $(call-cc-cpp-option,-Qunused-arguments)
+
 intermediates := $(call intermediates-dir-for,STATIC_LIBRARIES,libRS,TARGET,)
 librs_generated_headers := \
     $(intermediates)/rsgApiStructs.h \

Android.mk
@@ -1,7 +1,7 @@
 
 LOCAL_PATH:=$(call my-dir)
 
-rs_base_CFLAGS := -Werror -Wall -Wno-unused-parameter -Wno-unused-variable
+rs_base_CFLAGS := -Werror -Wall -Wno-unused-parameter -Wno-unused-variable $(call-cc-cpp-option,-Qunused-arguments)
 # For gcc 4.7, C++11 narrowing is a warning...
 rs_base_CFLAGS += -Wno-error
 ifeq ($(TARGET_BUILD_PDK), true)

commit 9aa1164cf5c26258cfec699c4d53f2d2e90813c1

rs: Fix RS_VERSION define
Commit 51dc7a27a8749d4ad43169bf39d4378529e132cd broke the RS_VERSION
define by setting LOCAL_CFLAGS := something after the addition of
LOCAL_CFLAGS += -DRS_VERSION=...

Make sure the := definition occurs before the += amendment.

Change-Id: I320ae9215f3ef2982c707a6ecaf5a14880ffa036
Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

cpp/Android.mk
@@ -11,6 +11,7 @@ else
 endif
 local_cflags_for_rs_cpp += -DRS_VERSION=$(RS_VERSION)
 
+LOCAL_CFLAGS := $(call-cc-cpp-option,-Qunused-arguments)
 LOCAL_CFLAGS += $(local_cflags_for_rs_cpp)
 
 LOCAL_SRC_FILES:= \
@@ -33,8 +34,6 @@ LOCAL_SHARED_LIBRARIES := \
 LOCAL_MODULE:= libRScpp
 
 LOCAL_MODULE_TAGS := optional
-
-LOCAL_CFLAGS := $(call-cc-cpp-option,-Qunused-arguments)
 
 intermediates := $(call intermediates-dir-for,STATIC_LIBRARIES,libRS,TARGET,)
 librs_generated_headers := \